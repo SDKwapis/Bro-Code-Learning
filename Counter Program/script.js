480c29817575d2f3e8fb53daf26648f3ba45f1ff
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-10-21 18:04:05 +0000

    Added us functionality to the counter program

--- Counter Program/script.js
+++ Counter Program/script.js
@@ -1,20 +1,45 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBttn");
-const increaseBtn = document.getElementById("increaseBttn");
 const countLabel = document.getElementById("countLabel");
+const myText = document.getElementById("myText");
+const myH1 = document.getElementById("myH1");
+const countContainer = document.getElementById("countContainer");
+const submitContainer = document.getElementById("submitContainer");
+
 let count = 0;
+let username = ""
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+countContainer.style.display = "none";
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
+document.getElementById("increaseBttn").addEventListener("click", countUp);
+document.getElementById("decreaseBttn").addEventListener("click", countDown);
+document.getElementById("resetBttn").addEventListener("click", reset);
+document.getElementById("submit").addEventListener("click", signIn);
+document.getElementById("signOut").addEventListener("click", signOut);
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
+function countUp() {
+  count++;
+  countLabel.innerHTML = count;
+};
+
+function countDown() {
+  count--;
+  countLabel.innerHTML = count;
+};
+
+function reset() {
+  count = 0;
+  countLabel.innerHTML = count;
+};
+
+function signIn() {
+  username = myText.value;
+  myH1.innerHTML = `Let's count, ${username}!`;
+  submitContainer.style.display = "none";
+  countContainer.style.display = "block";
+};
+
+function signOut() {
+  submitContainer.style.display = "block";
+  countContainer.style.display = "none";
+  myH1.innerHTML = "Welcome To The Magical World Of Counting!";
+  myText.value = "";
+};
