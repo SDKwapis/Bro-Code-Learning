743b1328ada7621f03982ba2154aeda326c0dc89
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-10-21 20:00:38 +0000

    Added some js functionality

--- To Do List/Script.js
+++ To Do List/Script.js
@@ -1,5 +1,11 @@
-document.getElementById("myButton").addEventListener("click", addTask)
+document.getElementById("myButton").addEventListener("click", addTask);
 
 function addTask() {
-  
+  const todoList = document.getElementById("myUL");
+  const newTask = document.createElement("li");
+  const task = document.getElementById("myText");
+
+  newTask.textContent = task.value;
+  todoList.appendChild(newTask);
+  task.value = "";
 }
