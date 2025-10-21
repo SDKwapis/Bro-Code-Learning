a8d3e17a9b574db06172c1948d562ea170dd2c25
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-10-21 20:14:52 +0000

    Added checkbox functionality

--- To Do List/Script.js
+++ To Do List/Script.js
@@ -4,8 +4,12 @@ function addTask() {
   const todoList = document.getElementById("myUL");
   const newTask = document.createElement("li");
   const task = document.getElementById("myText");
+  const checkbox = document.createElement("input");
 
+  checkbox.type = "checkbox";
   newTask.textContent = task.value;
   todoList.appendChild(newTask);
+  todoList.appendChild(checkbox);
   task.value = "";
-}
+};
+
