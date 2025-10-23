fd1e55f042c7023fb6e4079e57534c08a2884982
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-10-23 13:03:26 +0000

    Adding more functionality

--- To Do List/Script.js
+++ To Do List/Script.js
@@ -1,17 +1,19 @@
 document.getElementById("myButton").addEventListener("click", addTask);
 
 function addTask() {
+  if (document.getElementById("myText").value === "") {
+   console.log("nothing here")
+    return; 
+  } else {
   const todoList = document.getElementById("myUL");
   const newTask = document.createElement("li");
   const task = document.getElementById("myText");
   const checkbox = document.createElement("input");
 
-  // checkbox.type = "checkbox";
-
   newTask.textContent = task.value;
   todoList.appendChild(newTask);
   newTask.classList.add("listItem");
-  // todoList.appendChild(checkbox);
   task.value = "";
+}
 };
 
